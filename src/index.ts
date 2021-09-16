7c61160dab1236aed486c8f9bfb5d1282a6f36d8
yuchuan zhuang <yuchuan.zhuang@mindgeek.com>
2021-09-16 16:18:21 -0400

    typescript nodejs project demo

---
+++ src/index.ts
@@ -0,0 +1,54 @@
+import http, { IncomingMessage, ServerResponse } from "http";
+import path from "path";
+import fs from "fs/promises";
+import url from "url";
+import fetch from "node-fetch";
+
+interface Joke {
+  id: string;
+  joke: string;
+  status: number;
+}
+
+async function requestListener(req: IncomingMessage, res: ServerResponse) {
+  const parsedUrl = url.parse(req.url || "");
+
+  let data = "";
+  let statusCode = 200;
+  try {
+    let pathName = parsedUrl.pathname;
+    if (pathName === "/") pathName = "/index";
+    const filePath = path.join(__dirname, `static${pathName}.html`);
+    data = await fs.readFile(filePath, "utf-8");
+  } catch {
+    data = await fs.readFile(path.join(__dirname, "static/404.html"), "utf-8");
+    statusCode = 404;
+  }
+
+  if (parsedUrl.pathname === "/dad-joke") {
+    const response = await fetch("https://icanhazdadjoke.com", {
+      headers: {
+        accept: "application/json",
+        "user-agent": "NodeJS Server",
+      },
+    });
+    const joke: Joke = await response.json();
+
+    data = data.replace(/{{joke}}/gm, joke.joke);
+  }
+
+
+  res.writeHead(statusCode, {
+    "Content-type": "text/html",
+    "Content-Length": data.length,
+  });
+
+  res.write(data);
+  res.end();
+
+}
+
+http.createServer(requestListener).listen(3000, () => {
+  console.log('HTTP Server listening on port 3000');
+})
+
